a4119d3c84ac89e951ca49c35db4a5cd96a792f8
Michael-Jo <michjo2008@gmail.com>
2024-02-02 17:57:36 +0700

    main

---
+++ Informatika dan Program Koding/10 Program Koding/PYTHON/MEET08/ForLoops.py
@@ -0,0 +1,15 @@
+# FOR LOOP : range()
+
+for x in range(5):
+    print(f"LOOP range (5) : {x}")
+
+    print( "-"*15 )
+
+for z in range( 1, 11 ):
+    print(f"LOOP range (1, 11): {z}")
+
+    print( "-"*15 )
+for i in range( 1, 11, 2 ):
+    print(f"LOOP range (1, 11, 2): {i}")
+
+    print( "-"*15 )
