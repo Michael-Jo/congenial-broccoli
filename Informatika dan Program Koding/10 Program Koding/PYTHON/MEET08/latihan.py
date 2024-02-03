b24295c45f7f29f459e79adbb465633a24ba734e
Michael-Jo <michjo2008@gmail.com>
2024-02-03 09:10:56 +0700

    fix error

--- Informatika dan Program Koding/10 Program Koding/PYTHON/MEET08/latihan.py
+++ Informatika dan Program Koding/10 Program Koding/PYTHON/MEET08/latihan.py
@@ -2,6 +2,7 @@ print("\t\tTIKET BUS\t\t")
 print("-"*15)
 print("Kode kota :\n 1. Prabumulih \n 2. Muara Enim \n 3. Lubuklinggau")
 kota = float(input("Masukan pilihan kota\t: "))
+end_program = False
 if kota == 1 :
     print("Kode kelas:\n 1. Ekonomi \t(Rp. 100000) \n 2. Bisnis \t(Rp. 400000) \n 3. Eksekutif \t(Rp. 1000000) ")
     kelas = float(input("Masukan kelas\t: "))
@@ -13,6 +14,7 @@ if kota == 1 :
         harga = 1000000
     else:
         print("Masukan kelas salah")
+        end_program = True
 elif kota == 2:
     print("Kode kelas:\n 1. Ekonomi \t(Rp. 200000) \n 2. Bisnis \t(Rp. 500000) \n 3. Eksekutif \t(Rp. 800000) ")
     kelas = float(input("Masukan kelas\t: "))
@@ -24,6 +26,7 @@ elif kota == 2:
         harga = 800000
     else:
         print("Masukan kelas salah")
+        end_program = True
 elif kota == 3:
     print("Kode kelas:\n 1. Ekonomi \t(Rp. 300000) \n 2. Bisnis \t(Rp. 600000) \n 3. Eksekutif \t(Rp. 900000) ")
     kelas = float(input("Masukan kelas\t: "))
@@ -35,21 +38,24 @@ elif kota == 3:
         harga = 900000
     else:
         print("Masukan kelas salah")
+        end_program = True
 else:
     print("Masukan kota salah")
-jumlah = float(input("Masukan banyak tiket\t: "))
-subtotal = harga*jumlah
-if (kota == 2 and kelas == 1) or (kota == 3 and kelas == 3):
-    kode = input("Masukan kode promo\t: ")
-    if kode == "igs":
-        diskon = 0.1 * subtotal
-    else:
+    end_program = True
+if end_program == False:
+    jumlah = float(input("Masukan banyak tiket\t: "))
+    subtotal = harga*jumlah
+    if (kota == 2 and kelas == 1) or (kota == 3 and kelas == 3):
+        kode = input("Masukan kode promo\t: ")
+        if kode == "igs":
+            diskon = 0.1 * subtotal
+        else:
+            diskon = 0
+    else: 
         diskon = 0
-else: 
-    diskon = 0
-total = subtotal - diskon
-print("-"*15)
-print(f"Harga tiket\t: Rp. {harga:>15.0f}")
-print(f"Subtotal\t: Rp. {subtotal:>15.0f}")
-print(f"Diskon (10%)\t: Rp. {diskon:>15.0f}")
-print(f"Total\t\t: Rp. {total:>15.0f}")
+    total = subtotal - diskon
+    print("-"*15)
+    print(f"Harga tiket\t: Rp. {harga:>15.0f}")
+    print(f"Subtotal\t: Rp. {subtotal:>15.0f}")
+    print(f"Diskon (10%)\t: Rp. {diskon:>15.0f}")
+    print(f"Total\t\t: Rp. {total:>15.0f}")
