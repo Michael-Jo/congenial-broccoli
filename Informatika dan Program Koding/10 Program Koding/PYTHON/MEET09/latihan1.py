ef7195dc653c51b15af01f6df02dfde65d451607
Michael-Jo <michjo2008@gmail.com>
2024-02-03 10:26:28 +0700

    added 2 latihan

---
+++ Informatika dan Program Koding/10 Program Koding/PYTHON/MEET09/latihan1.py
@@ -0,0 +1,7 @@
+# Kumpul di moodle, by Michael Jonathan Sandy
+n = int(input("Masukan bilangan :"))
+while n > 20 and n % 2 = 1:
+    print("Bilangan salah !")
+    n = int(input("Masukan bilangan :"))
+else:
+    print("Bilangan benar")
